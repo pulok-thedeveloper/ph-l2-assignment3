df4610946ae6ffd2b353b6ede5e0b14e4303b7cc
pulok-thedeveloper <mdpulokhasan@gmail.com>
2025-09-07 22:21:00 +0600

    cors origin update

--- dist/app.js
+++ dist/app.js
@@ -4,10 +4,26 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const book_controllers_1 = require("./app/controllers/book.controllers");
 const borrow_controllers_1 = require("./app/controllers/borrow.controllers");
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
+const allowedOrigins = [
+    "http://localhost:5173", // dev
+    "https://library-management-frontend-mauve.vercel.app", // production
+];
+app.use((0, cors_1.default)({
+    origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        }
+        else {
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    credentials: true,
+}));
 app.use("/api/books", book_controllers_1.bookRoutes);
 app.use("/api/borrow", borrow_controllers_1.borrowRoutes);
 app.get("/", (req, res) => {
--- dist/server.js
+++ dist/server.js
@@ -5,14 +5,13 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = __importDefault(require("./app"));
 const mongoose_1 = __importDefault(require("mongoose"));
+const config_1 = __importDefault(require("./config"));
 let server;
-const PORT = process.env.PORT || 5000;
-const URI = "mongodb+srv://dbUser1:880032a3e0@cluster0.tftz42f.mongodb.net/library-management?retryWrites=true&w=majority&appName=Cluster0";
 async function main() {
     try {
-        await mongoose_1.default.connect(`${URI}`);
-        server = app_1.default.listen(PORT, () => {
-            console.log(`Server is running on port ${PORT}`);
+        await mongoose_1.default.connect(`${config_1.default.database_url}`);
+        server = app_1.default.listen(config_1.default.port, () => {
+            console.log(`Server is running on port ${config_1.default.port}`);
         });
     }
     catch (error) {
